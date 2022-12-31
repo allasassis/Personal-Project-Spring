dbbebc8980addcd27cce3b845209e4bb05636ef9
Allas Assis <allasassis10@gmail.com>
2022-12-30 23:06:46 -0300

    Method getById and Insert created

--- src/main/java/com/allasassis/bank/controllers/CustomerController.java
+++ src/main/java/com/allasassis/bank/controllers/CustomerController.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,5 +24,16 @@ public class CustomerController {
 	public List<Customer> findAll() {
 		List<Customer> list = customerRepository.findAll();
 		return list;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public Customer findById(@PathVariable Long id) {
+		Customer customer = customerRepository.findById(id).get();
+		return customer;
+	}
+	
+	@PostMapping
+	public void insertCustomer(@RequestBody Customer customer) {
+		customerRepository.save(customer);
 	}
 }
