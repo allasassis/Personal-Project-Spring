953f99d8ab436257176925f9123fd6ae44f66033
Allas Assis <allasassis10@gmail.com>
2022-12-31 15:13:52 -0300

    Account fixed

--- src/main/java/com/allasassis/bank/entities/Account.java
+++ src/main/java/com/allasassis/bank/entities/Account.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -24,7 +24,7 @@ public class Account implements Serializable{
 	private String type;
 	// private Double balance;
 	
-	@ManyToOne
+	@OneToOne
 	@JoinColumn(name = "customer_id")
 	private Customer customer;
 	
